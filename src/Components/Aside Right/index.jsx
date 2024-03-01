496434f43d8627afc03157b2f860911579b3a53c
albores339 <juandediosalboreshernandez@gmail.com>
2024-02-29 21:14:32 -0600

    Fetch

--- src/Components/Aside Right/index.jsx
+++ src/Components/Aside Right/index.jsx
@@ -1,6 +1,6 @@
 export const AsideRight = () => {
     return(
-        <div>
+        <div className="bg-sky-500 border border-stone-400 m-32 p-8">
             <h1>Right</h1>
         </div>
 
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,19 +1,18 @@
 import { Navbar } from "../../Components/NavBar"
 import { Footer } from "../../Components/footer"
-import { AsideLeft } from "../../Components/AsideLeft"
+import { AsideLeft } from "../../Components/Aside Left"
 import { AsideRight } from "../../Components/Aside Right"
 import { Posts } from "../../Components/Posts"
 
 export const Home = () => {
     return(
-        <div>
+        <div className="flex flex-column  gap-8 w-full bg-stone-100">
             <Navbar/>
-                <h1>Hola</h1>
-                <div className="flex flex-row gap-8 w-full max-w-screen-lg p-2">
-                    <AsideLeft/>
-                    <Posts/>
-                    <AsideRight/>
-                </div>
+            <div className="flex flex-row gap-8">
+                <AsideLeft/>
+                <Posts/>
+                <AsideRight/>
+            </div>
             <Footer/>
         </div>
 
